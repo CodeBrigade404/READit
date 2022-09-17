5c7eb60da37b055b26e6ab84d6d02bee0ea6251f
IT21019968 <it21019968@my.sliit.lk>
2022-09-17 10:37:29 +0530

    Created all design views of the login registration and the Account

---
+++ app/src/main/java/com/example/readit/SignUp_Otp.java
@@ -0,0 +1,14 @@
+package com.example.readit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUp_Otp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_otp);
+    }
+}
